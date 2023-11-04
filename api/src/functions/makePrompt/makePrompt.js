833521dc1eeaef230bef10bf6f4c835c78082fe9
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2023-11-04 13:42:23 -0400

    GPT prompt API

--- api/src/functions/makePrompt/makePrompt.js
+++ api/src/functions/makePrompt/makePrompt.js
@@ -1,6 +1,7 @@
 import { logger } from 'src/lib/logger'
 import { OpenAI } from "langchain/llms/openai";
 import { PromptTemplate } from "langchain/prompts";
+require('dotenv').config()
 /**
  * The handler function is your code that processes http request events.
  * You can use return and throw to send a response or error, respectively.
@@ -17,26 +18,38 @@ import { PromptTemplate } from "langchain/prompts";
  * @param { Context } context - contains information about the invocation,
  * function, and execution environment.
  */
-let KEY = process.env.USER_MY_OPENAI_API_KEY?process.env.OPENAI_API_KEY:"";
-let prompt = await JSON.parse(event.body).prompt;//{ prompt: "Tell me a joke about bears" };
+let KEY = process.env.USE_MY_OPENAI_API_KEY?process.env.OPENAI_API_KEY:"";
+
+
+async function getPrompt(prompt) {
 const model = new OpenAI({
   modelName: "text-davinci-003", // Defaults to "text-davinci-003" if no model provided.
   temperature: 0.9,
   openAIApiKey: KEY, // In Node.js defaults to process.env.OPENAI_API_KEY
 });
-const res = await model.call(
-  "What would be a good company name a company that makes colorful socks?"
-);
+const res = await model.call(prompt);
+  logger.info(res.toString());
+  return res;
+}
+
+
 export const handler = async (event, _context) => {
-
-
+  let prompt = (JSON.parse(event.body)).prompt;//{ prompt: "Tell me a joke about bears" };
+  logger.info(prompt);
+  if (!prompt) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'No prompt provided' }),
+    }
+  }
+  let response = getPrompt(prompt);
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      data: result,
+      data: (await response).toString(),
     }),
   }
 }
