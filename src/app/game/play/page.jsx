e99478c148b2985e3ed5b8be385a75a13bb18057
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2023-11-05 08:23:42 -0500

    Add server-side functionality for saving player responses to game prompts

--- src/app/game/play/page.jsx
+++ src/app/game/play/page.jsx
@@ -1,10 +1,33 @@
 import { newPrompt } from "@/lib/newPrompt/newPrompt";
-
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import {cookies} from "next/headers";
+import { savePlayerResponseToRound } from "./action";
 export default async function Page() {
+  const supabase = createServerComponentClient({ cookies });
 let data = await newPrompt([
     "if you were stranded on a desert island, what would you bring?"
 ],"A boat");
 data=JSON.parse(data);
+let userId = cookies().get('userId').value
+
+
+let {data: playerGamesData} = await supabase
+.from('PlayerGames')
+.select('gameId')
+.eq('userId', userId)
+.single()
+// let {data: gameData} = await supabase
+// .from('Game')
+// .select('game_code')
+// .eq('id', playerGamesData.gameId)
+// .single()
+const inserted = await supabase
+  .from('GamePrompt')
+  .insert([
+    { prompt: data.result,gameId: playerGamesData.gameId, responderId:userId},
+  ])
+  .select()
+
 return(
     
 <>
@@ -13,9 +36,10 @@ return(
 {data.result}
 </h1>
 </div>
-<form className="form-control flex flex-col gap-4 px-16  justify-center">
-
-<input type="text" name="response" id="response" className="form-input py-4 border-black rounded-md indent-5" placeholder="Response goes here" />
+<form className="form-control flex flex-col gap-4 px-16  justify-center" action={savePlayerResponseToRound}>
+<input type="hidden" name="questionId" id="questionId" value={inserted.data[0].id} />
+<input type="text" name="userResponse" id="response" className="form-input py-4 border-black rounded-md indent-5" placeholder="Response goes here" />
+<button type="submit" className="btn btn-primary">Submit</button>
 </form>
 
 </>    
