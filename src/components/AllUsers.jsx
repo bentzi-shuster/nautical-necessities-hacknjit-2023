49ef09e6483c0ae04151778ae9eee96cb844b21e
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2023-11-05 01:58:56 -0500

    fixes

--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -69,7 +69,7 @@ export default function AllUsers({gameId, allPlayers}) {
                     {
                         allUsers.map((data, index) => {
                             return (
-                                <PlayerCard greyedOut={true} key={index} name={data.name} width={100} height={100} />
+                                <PlayerCard key={index} name={data.name} width={100} height={100} />
                             )
                         })
                     }</div>
